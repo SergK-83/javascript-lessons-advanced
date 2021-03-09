d04b611cd29d753be301c47ed733d4ee37c62deb
Сергей Крюков <krus-83@mail.ru>
2021-03-09 16:59:16 +0700

    lesson ES6 classes

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const person = new Object({
     }
 });
 
-// Object.prototype.sayHello = function () {
-//     console.log('Hello');
-// };
-//
-// const lena = Object.create(person);
-//
-// lena.name = 'Elena';
-//
-// const str = new String('I am string');
+Object.prototype.sayHello = function () {
+    console.log('Hello');
+};
+
+const lena = Object.create(person);
+
+lena.name = 'Elena';
+
+const str = new String('I am string');
 
 // console.log(str.sayHello());
 
@@ -268,4 +268,20 @@ const person5 = Object.create(
 
 // Все о ES6 Классах
 
+class Animal {
+    constructor(options) {
+        this.name = options.name;
+        this.age = options.age;
+        this.hasTail = options.hasTail;
+    }
 
+    voice() {
+        console.log('I am Animal');
+    }
+}
+
+const animal = new Animal({
+   name: 'Animal',
+   age: 5,
+   hasTail: true
+});
